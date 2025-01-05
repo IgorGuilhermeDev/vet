74af479d8f65f783963c988c08fc44e8ef7263ae
Igor Guilherme Almeida Rocha <igorguilhermedev@gmail.com>
2025-01-05 20:46:24 -0300

    chore: first setup

---
+++ src/test/java/org/com/vet/VeterinaryOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.vet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
